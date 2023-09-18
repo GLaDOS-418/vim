1b6ae66e3fc65bed25c5fba7ad73b5b4e875f6b9
GLaDOS-418 <arnobbhanja@live.com>
2023-09-18 17:41:59 +0530

    neotree config

---
+++ nvim/lua/neotree_cfg.lua
@@ -0,0 +1,10 @@
+require("neo-tree").setup({
+  window = {
+    mappings = {
+      ["P"] = function(state)
+        local node = state.tree:get_node()
+        require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+      end
+    }
+  }
+})
