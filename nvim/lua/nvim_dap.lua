fff040deca2a007c91b2d709936f62663d8d58f4
GLaDOS-418 <arnobbhanja@live.com>
2024-12-25 16:51:08 +0530

    [WIP] some nvim-dap shit.

--- nvim/lua/nvim_dap.lua
+++ nvim/lua/nvim_dap.lua
@@ -2,39 +2,67 @@
 -- https://github.com/mfussenegger/nvim-dap
 ----------------------------------------------
 local dap = require('dap')  -- required by nvim-dap-ui as well
-dap.adapters.lldb = {
-  type = 'executable',
-  command = '/usr/bin/lldb-vscode-17', -- must be absolute path
-  name = 'lldb'
+
+----------------------------------------------
+-- KEYMAPS
+----------------------------------------------
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<F9>', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
+
+----------------------------------------------
+-- LANGUAGE SPECIFIC
+----------------------------------------------
+--- C++
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    command = '/home/glados/thirdparty/codelldb/extension/adapter/codelldb',
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
 }
 
 dap.configurations.cpp = {
   {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    -- program = function()
-    --   return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build', 'file')
-    -- end,
+    name = "Debug",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
-    args = {},
-
-    -- 💀
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    -- runInTerminal = false,
   },
 }
 
+--- C
+dap.configurations.c = dap.configurations.cpp
+
+--- RUST
+dap.configurations.rust = dap.configurations.cpp
 
 --------------------------------------------
 -- https://github.com/rcarriga/nvim-dap-ui
